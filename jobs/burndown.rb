13744d576c5dfdd40303a4983810ad7d61b58485
Diogo Key <diogo.key@mowa.com.br>
2015-01-09 19:42:16 -0200

    Changes the URL of the API and removes the Owners Widget

--- jobs/burndown.rb
+++ jobs/burndown.rb
@@ -3,7 +3,7 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
   print "Extracting series data and constructing widget event message...\n"
   # points = [{x:1, y:4}, {x:2, y:27}, {x:3, y:6}]
   # send_event('burndown', points: points)
-  url = URI.parse("http://ninenine-stats.herokuapp.com/dakar/dashboard?year=2015&month=1&goal=4000")
+  url = URI.parse("http://ninenine-stats.herokuapp.com/dakar/evaluations?year=2015&month=1&goal=4000")
   http = Net::HTTP.new(url.host, url.port)
   response = http.request(Net::HTTP::Get.new(url.request_uri))
 
@@ -11,5 +11,5 @@ SCHEDULER.every '30s', :first_in => 0 do |job|
   j = JSON[response.body]
 
 
-  send_event('burndown', series: j["series"])
+  send_event('burndown_evaluations', series: j["series"])
 end
