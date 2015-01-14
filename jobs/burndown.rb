b45ee8430e544f0b514f23f1e30ab2ff6c693b75
Diogo Key <diogo.key@mowa.com.br>
2015-01-14 17:13:28 -0200

    Setting the schedule for every 30m

--- jobs/burndown.rb
+++ jobs/burndown.rb
@@ -3,7 +3,7 @@ require 'pry'
 current_evaluations = 0
 current_opportunities = 0
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '10s', :first_in => 0 do |job|
+SCHEDULER.every '30m', :first_in => 0 do |job|
 
 ################## EVALUATIONS BELOW ##################  
 
